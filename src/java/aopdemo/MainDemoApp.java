062a2a93b8d905cc434a0712e9802273cdb3d567
Mateonidas <teus.polec@gmail.com>
2020-04-10 18:25:01 +0200

    Test of @Before advice.

---
+++ src/java/aopdemo/MainDemoApp.java
@@ -0,0 +1,17 @@
+package aopdemo;
+
+import aopdemo.dao.AccountDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        accountDAO.addAccount();
+
+        context.close();
+    }
+}
---
+++ src/java/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,11 @@
+package aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+
+    public void addAccount(){
+        System.out.println(getClass() + ": ADDING ACCOUNT");
+    }
+}
